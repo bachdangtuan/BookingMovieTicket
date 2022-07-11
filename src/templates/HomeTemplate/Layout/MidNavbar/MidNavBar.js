07da6dfb3a8e21da3829150e5309b4644ddf91a5
DarKTuan <bachdangtuan93@gmail.com>
2022-07-11 21:21:35 +0700

    update

--- src/templates/HomeTemplate/Layout/MidNavbar/MidNavBar.js
+++ src/templates/HomeTemplate/Layout/MidNavbar/MidNavBar.js
@@ -4,10 +4,8 @@ export default function MidNavBar() {
     return (
         <div className='text-center 2xl:container xl:m-auto'>
             <ul className='justify-center align-middle '>
-                <li>
-                    
-                    <img src='https://www.cgv.vn/skin/frontend/cgv/default/images/bg-cgv/bg-header-transparent-home.png' className='m-auto'></img>
-                                    </li>
+                <li><img src='https://www.cgv.vn/skin/frontend/cgv/default/images/bg-cgv/bg-header-transparent-home.png' className='m-auto'></img>
+                </li>
             </ul>
         </div>
     )
